cd40e902f0ed595349725cc9f3e1d5773bba17dc
Eric Normand <ericwnormand@gmail.com>
2019-01-30 11:24:26 -0600

    Upgrade Clojure, ClojureScript to latest

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://purelyfunctional.tv/"
   :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.908"]]
-  :plugins [[lein-doo "0.1.7"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.439"]]
+  :plugins [[lein-doo "0.1.10"]]
 
   :cljsbuild {:builds
               {:browser-test {:source-paths ["cljs-src" "cljs-test"]
                               :compiler {:output-to "out/browser_tests.js"
-                                         :main 'lab-notebook.browser
+                                         :main lab-notebook.browser
                                          :optimizations :none}}}})
